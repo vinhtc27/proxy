d6364adc2a41969139147fbeea0a55ab061ebd13
Khanh Linh <khanhlinh.ph2002@gmail.com>
2023-10-25 00:34:35 +0700

    fixed window algorithm

---
+++ algorithm/fixed_window/window.go
@@ -0,0 +1,40 @@
+package fw
+
+import (
+	"sync"
+	"time"
+)
+
+type window struct {
+	limit           int
+	windowSize      time.Duration
+	requestCount    int
+	lastRequestTime time.Time
+	mu              sync.Mutex
+}
+
+func newWindow(limit int, windowSize time.Duration) *window {
+	return &window {
+		limit:           limit,
+		windowSize:      windowSize,
+		requestCount:    0,
+		lastRequestTime: time.Now(),
+	}
+}
+
+func (w *window) Allow() bool {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	now := time.Now()
+	if now.Sub(w.lastRequestTime) > w.windowSize {
+		w.lastRequestTime = now
+		w.requestCount = 1
+		return true
+	}
+	if w.requestCount < w.limit {
+		w.requestCount++
+		return true
+	}
+	return false
+}
